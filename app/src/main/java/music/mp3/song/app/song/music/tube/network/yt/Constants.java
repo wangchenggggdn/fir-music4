2d2e1c25f6f5594a81dadefc15ad5418aec429f2
wangchenggggdn <wangchenggggdn@163.com>
2025-08-01 17:31:58 +0800

    新建工程【完全跑通】

---
+++ app/src/main/java/music/mp3/song/app/song/music/tube/network/yt/Constants.java
@@ -0,0 +1,29 @@
+package music.mp3.song.app.song.music.tube.network.yt;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+import org.schabi.newpipe.extractor.localization.ContentCountry;
+import org.schabi.newpipe.extractor.localization.Localization;
+
+public class Constants {
+    public static final int NO_SERVICE_ID = -1;
+
+    public static org.schabi.newpipe.extractor.localization.Localization getPreferredLocalization(final Context context) {
+        final String contentLanguage = PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString("content_language",
+                        "en");
+        return Localization.fromLocalizationCode(contentLanguage).get();
+    }
+
+    public static ContentCountry getPreferredContentCountry(final Context context) {
+        final String contentCountry = PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString("content_country",
+                        "GB");
+        return new ContentCountry(contentCountry);
+    }
+
+
+}
---
+++ encrypt/src/main/java/encrypt/pck/JiaMi.java
@@ -0,0 +1,11 @@
+package encrypt.pck;
+
+public class JiaMi {
+    public static String applovin_sdk_key = "wBoUPtps7bLwU5xddHu9qMTzdsScLZ3MI9EwyECTk0ZLxKMBVdpTTPBosSTFDrbuzxb3-4CmK5Fo-QR8DNl-15";
+
+    public static String configBaseUrl = "https://firmusic.oss-us-west-1.aliyuncs.com/";
+
+    public static String ban = "cn-hk-sg-in-ru-jp-kr-tur-uk-tr-jb-ind-chn-fr-fra-gb";
+
+    public static String speciallanguage = "pt-es";
+}
